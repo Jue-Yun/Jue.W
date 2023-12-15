9aed07338a222f0feac9e9217eebcd05230abf59
Uncle Zheng <inmount@live.cn>
2023-12-15 18:54:59 +0800

    修改：清理无用代码

--- vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
-import Jues from './plugins/jues'
-
-const app = createApp(App)
-installElementPlus(app)
-Jues.install(app)
-app.mount('#app')
